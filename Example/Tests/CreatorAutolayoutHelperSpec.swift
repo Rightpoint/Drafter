ac3a753b292a7b5ae17c61d88e7a596ee3b4bc38
mattThousand <matt.d.buckley1212@gmail.com>
2015-08-15 10:55:57 -0400

    Clean up CreatorAutolayoutHelperSpec.

--- Example/Tests/CreatorAutolayoutHelperSpec.swift
+++ Example/Tests/CreatorAutolayoutHelperSpec.swift
@@ -22,41 +22,43 @@ class CreatorAutolayoutHelperSpec: QuickSpec {
             self.superview.addSubview(self.view)
         })
 
+        beforeEach({
+            expect(self.superview.constraints).to(equal([]))
+        })
+
+        afterEach({
+            self.superview.removeConstraints(self.view.constraints)
+        })
+
         describe("CreatorAutoLayoutHelpers", {
 
             context("pinSideInactive", {
-                beforeEach({
-                    expect(self.superview.constraints).to(equal([]))
-                })
 
-                it("doesn't put the left constraint to active", closure: {
+                it("doesn't set the left constraint to active", closure: {
                     let constraint = self.view.pinLeftSpaceToSuperview(isActive: false)
                     expect(self.view.translatesAutoresizingMaskIntoConstraints).to(equal(false))
                     expect(constraint.active).to(equal(false))
                     expect(self.superview.constraints.count).to(equal(0))
                 })
-                it("doesn't put the right constraint to active", closure: {
+                it("doesn't set the right constraint to active", closure: {
                     let constraint = self.view.pinRightSpaceToSuperview(isActive: false)
                     expect(self.view.translatesAutoresizingMaskIntoConstraints).to(equal(false))
                     expect(constraint.active).to(equal(false))
                     expect(self.superview.constraints.count).to(equal(0))
                 })
-                it("doesn't put the bottom constraint to active", closure: {
+                it("doesn't set the bottom constraint to active", closure: {
                     let constraint = self.view.pinBottomSpaceToSuperview(isActive: false)
                     expect(self.view.translatesAutoresizingMaskIntoConstraints).to(equal(false))
                     expect(constraint.active).to(equal(false))
                     expect(self.superview.constraints.count).to(equal(0))
                 })
-                it("doesn't put the top constraint to active", closure: {
+                it("doesn't set the top constraint to active", closure: {
                     let constraint = self.view.pinTopSpaceToSuperview(isActive: false)
                     expect(self.view.translatesAutoresizingMaskIntoConstraints).to(equal(false))
                     expect(constraint.active).to(equal(false))
                     expect(self.superview.constraints.count).to(equal(0))
                 })
 
-                afterEach({
-                    self.superview.removeConstraints(self.view.constraints)
-                })
             })
         })
     }
