27804babb1d7fd7a65d370ccbdaffb3b1f418dcb
Derek Ostrander <djostran@gmail.com>
2015-08-25 15:50:40 -0400

    Better centerers

--- Pod/Classes/CenteringAutoLayoutHelpers.swift
+++ Pod/Classes/CenteringAutoLayoutHelpers.swift
@@ -11,8 +11,25 @@ import UIKit
 public extension UIView {
 
     // MARK: Public Interface
-    public func centerInContainer (forAxis axis:UILayoutConstraintAxis, offset: CGFloat = 0.0, priority:UILayoutPriority = UILayoutPriorityRequired) -> NSLayoutConstraint {
+    public func centerInContainer(offset: CGPoint = CGPoint.zeroPoint,
+        isActive active: Bool = true,
+        priority:UILayoutPriority = UILayoutPriorityRequired) -> (x: NSLayoutConstraint, y: NSLayoutConstraint) {
+            let x = centerInContainer(forAxis: .Horizontal, offset: offset.x, isActive: false, priority: priority)
+            let y = centerInContainer(forAxis: .Vertical, offset: offset.y, isActive: false, priority: priority)
+            if active {
+                NSLayoutConstraint.activateConstraints([x,y])
+            }
 
-        return centerInContainer(isActive: false, axis: axis, offset: offset).activate(priority)
+            return (x, y)
+    }
+
+    public func centerInContainer(forAxis axis:UILayoutConstraintAxis,
+        offset: CGFloat = 0.0,
+        isActive active: Bool = true,
+        priority:UILayoutPriority = UILayoutPriorityRequired) -> NSLayoutConstraint {
+            let constraint = centerInContainer(isActive: false, axis: axis, offset: offset).activate(priority)
+            constraint.priority = priority
+            constraint.active = active
+            return constraint
     }
 }
